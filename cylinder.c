fe4c7f53be6ec798957444d1f63ef0852d9d0a2f
Sam McSweeney <sammy.mcsweeney@gmail.com>
2018-02-22 13:40:55 +0800

    Finished the man pages

--- cylinder.c
+++ cylinder.c
@@ -1,7 +1,7 @@
 #include <math.h>
 #include "psrgeom.h"
 
-void move_around_cyl( point *start_pt, psr_angle *xi, double zdist,
+void move_around_cyl( point *start_pt, psr_angle *xi, double dist,
                       point *end_pt )
 /* From a starting point, step around the cylinder a fixed distance at a
  * specified angle. This function assumes that the starting point has
@@ -10,7 +10,7 @@ void move_around_cyl( point *start_pt, psr_angle *xi, double zdist,
  * Inputs:
  *   start_pt : the starting point
  *   xi       : the angle the trajectory makes with the xy-plane
- *   zdist    : the distance to travel
+ *   dist     : the distance to travel
  *
  * Outputs:
  *   end_pt   : the final point
@@ -20,9 +20,9 @@ void move_around_cyl( point *start_pt, psr_angle *xi, double zdist,
     double rh = start_pt->r * start_pt->th.sin; // = r sin θ
     double ph = start_pt->ph.rad * rh;
 
-    double znew = start_pt->x[2] + zdist*xi->sin;
+    double znew = start_pt->x[2] + dist*xi->sin;
     psr_angle phnew;
-    set_psr_angle_rad( &phnew, (ph + zdist*xi->cos)/rh );
+    set_psr_angle_rad( &phnew, (ph + dist*xi->cos)/rh );
 
     set_point_cyl( end_pt, rh, &phnew, znew, POINT_SET_ALL );
 }
