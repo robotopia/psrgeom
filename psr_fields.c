97f6a78c36a4b4d9bac33ec90fe0e51ba5184961
Sam McSweeney <sammy.mcsweeney@gmail.com>
2018-03-05 16:54:59 +0800

    Still working on psr_fields. Not finished yet.

--- psr_fields.c
+++ psr_fields.c
@@ -18,14 +18,29 @@ struct opts
     char   *format;    // output format string
     int     grid_type; // grid type (cart, cyl2, or cyl3)
     int     rL_norm;   // bool: normalise to light cylinder radius?
+    int     rL_lim;    // bool: limit to within light cylinder?
     int     npoints;   // number of points on one side of grid
     char   *outfile;   // name of output file (NULL means stdout)
     double  rho_max;   // largest rho to consider for gridpoints
     double  vsize;     // ratio of vector size to grid cell size
 };
 
+#define MAX_NTOKENS  64
+
+struct tokens {
+    char token[MAX_NTOKENS][3];
+    int calcB;
+    int calcV;
+    int calcA;
+    int n;
+};
+
 void usage();
 void parse_cmd_line( int argc, char *argv[], struct opts *o );
+void parse_format( char *format, struct tokens *tok );
+
+void print_col_headers( FILE *f, char *format );
+void print_token_values( FILE *f, struct tokens *tok );
 
 int main( int argc, char *argv[] )
 {
@@ -37,6 +52,7 @@ int main( int argc, char *argv[] )
     o.format    = NULL;
     o.grid_type = GT_CART;
     o.rL_norm   = 0;
+    o.rL_lim    = 0;
     o.npoints   = 0; /* Set to 0 now, because the true default depends on
                         value of grid_type */
     o.outfile   = NULL;
@@ -58,6 +74,14 @@ int main( int argc, char *argv[] )
             exit(EXIT_FAILURE);
         }
     }
+
+    // Set up tokens
+    struct tokens tok;
+    tok.calcB = 0;
+    tok.calcV = 0;
+    tok.calcA = 0;
+    tok.n     = 0;
+    parse_format( o.format, &tok );
 
     // Set up pulsar
     pulsar psr;
@@ -83,40 +107,109 @@ int main( int argc, char *argv[] )
     // Get the number of velocity solutions
     int nsols;
 
-    // Figure our grid spacing
-    double dx     = 2.0 * o.rho_max * psr.RL / (double)(npoints - 1);
-    double dtheta = 2.0 * o.rho_max * PI     / (double)(npoints);
 
     point B;
     point V1, V2;
     point A1, A2;
 
     print_psrg_header( f, argc, argv );
-    fprintf( f, "#x y z Bx By Bz V1x V1y V1z V2x V2y V2z A1x A1y A1z A2x A2y A2z\n" );
 
-    int i, j, k;
-    for (i = -1; i <= 1; i += 0.2)
-    for (j = -1; j <= 1; j += 0.2)
-    for (k = -1; k <= 1; k += 0.2)
+    if (o.grid_type == GT_CART)
     {
-        set_point_xyz( &X, (double)i*psr.rL,
-                           (double)j*psr.rL,
-                           (double)k*psr.rL,
-                           POINT_SET_ALL );
+        print_col_headers( f, o.format );
 
-        if (X.rhosq > psr.rL2) continue;
+        // Figure out grid spacing
+        double dx    = 2.0 * o.rho_max * psr.rL / (double)(o.npoints - 1);
+        double xmin  = -o.rho_max * psr.rL;
+        double vscale = o.vsize * dx;
+        double xscale = (o.rL_norm ? 1.0/psr.rL : 1.0);
 
-        calc_fields( &X, &psr, v, &B, &V1, &V2, &A1, &A2, &nsols );
-
-        if (nsols > 0)
+        int i, j, k;
+        for (i = 0; i < o.npoints; i++)
+        for (j = 0; j < o.npoints; j++)
+        for (k = 0; k < o.npoints; k++)
         {
-            fprintf( f, "%lf %lf %lf  ", i, j, k );
-            fprintf( f, "%.15e %.15e %.15e  ",  B.x[0],  B.x[1],  B.x[2] );
-            fprintf( f, "%.15e %.15e %.15e  ", V1.x[0], V1.x[1], V1.x[2] );
-            fprintf( f, "%.15e %.15e %.15e  ", V2.x[0], V2.x[1], V2.x[2] );
-            fprintf( f, "%.15e %.15e %.15e  ", A1.x[0], A1.x[1], A1.x[2] );
-            fprintf( f, "%.15e %.15e %.15e\n", A2.x[0], A2.x[1], A2.x[2] );
+            // Set the point at this gridpoint
+            set_point_xyz( &X, (double)i*dx + xmin,
+                               (double)j*dx + xmin,
+                               (double)k*dx + xmin,
+                               POINT_SET_ALL );
+
+            // If selected, ignore points outside of light cylinder
+            if (o.rL_lim && (X.rhosq > psr.rL2))
+                continue;
+
+            if (tok.calcA)
+                calc_fields( &X, &psr, v, &B, &V1, &V2, &A1, &A2, &nsols );
+            else if (tok.calcV)
+                calc_fields( &X, &psr, v, &B, &V1, &V2, NULL, NULL, &nsols );
+            else if (tok.calcB)
+                calc_fields( &X, &psr, v, &B, NULL, NULL, NULL, NULL, &nsols );
+
+            int n;
+            for (n = 0; n < tok.n; n++)
+            {
+                // Print X's
+                if (!strncmp( tok.token[n], "Xx", 2 ))
+                    fprintf( f, "%.15e ", xscale*X.x[0] );
+                if (!strncmp( tok.token[n], "Xy", 2 ))
+                    fprintf( f, "%.15e ", xscale*X.x[1] );
+                if (!strncmp( tok.token[n], "Xz", 2 ))
+                    fprintf( f, "%.15e ", xscale*X.x[2] );
+
+                // Print B's
+                if (!strncmp( tok.token[n], "Bx", 2 ))
+                    fprintf( f, "%.15e ", vscale*B.x[0] );
+                if (!strncmp( tok.token[n], "By", 2 ))
+                    fprintf( f, "%.15e ", vscale*B.x[1] );
+                if (!strncmp( tok.token[n], "Bz", 2 ))
+                    fprintf( f, "%.15e ", vscale*B.x[2] );
+
+                // Print V's
+                if (!strncmp( tok.token[n], "Vx1", 3 ))
+                    fprintf( f, "%.15e ", vscale*V1.x[0] );
+                if (!strncmp( tok.token[n], "Vy1", 3 ))
+                    fprintf( f, "%.15e ", vscale*V1.x[1] );
+                if (!strncmp( tok.token[n], "Vz1", 3 ))
+                    fprintf( f, "%.15e ", vscale*V1.x[2] );
+                if (!strncmp( tok.token[n], "Vx2", 3 ))
+                    fprintf( f, "%.15e ", vscale*V2.x[0] );
+                if (!strncmp( tok.token[n], "Vy2", 3 ))
+                    fprintf( f, "%.15e ", vscale*V2.x[1] );
+                if (!strncmp( tok.token[n], "Vz2", 3 ))
+                    fprintf( f, "%.15e ", vscale*V2.x[2] );
+
+                // Print A's
+                if (!strncmp( tok.token[n], "Ax1", 3 ))
+                    fprintf( f, "%.15e ", vscale*A1.x[0] );
+                if (!strncmp( tok.token[n], "Ay1", 3 ))
+                    fprintf( f, "%.15e ", vscale*A1.x[1] );
+                if (!strncmp( tok.token[n], "Az1", 3 ))
+                    fprintf( f, "%.15e ", vscale*A1.x[2] );
+                if (!strncmp( tok.token[n], "Ax2", 3 ))
+                    fprintf( f, "%.15e ", vscale*A2.x[0] );
+                if (!strncmp( tok.token[n], "Ay2", 3 ))
+                    fprintf( f, "%.15e ", vscale*A2.x[1] );
+                if (!strncmp( tok.token[n], "Az2", 3 ))
+                    fprintf( f, "%.15e ", vscale*A2.x[2] );
+
+            }
+
+            if (nsols > 0)
+            {
+                fprintf( f, "%.15e %.15e %.15e  ",  X.x[0],  X.x[1],  X.x[2] );
+                fprintf( f, "%.15e %.15e %.15e  ",  scale*B.x[0],  scale*B.x[1],  scale*B.x[2] );
+                fprintf( f, "%.15e %.15e %.15e  ", V1.x[0], V1.x[1], V1.x[2] );
+                fprintf( f, "%.15e %.15e %.15e  ", V2.x[0], V2.x[1], V2.x[2] );
+                fprintf( f, "%.15e %.15e %.15e  ", A1.x[0], A1.x[1], A1.x[2] );
+                fprintf( f, "%.15e %.15e %.15e\n", A2.x[0], A2.x[1], A2.x[2] );
+            }
         }
+    }
+    else
+    {
+        double dtheta = 2.0 * PI / (double)(o.npoints);
+        double dx     = dtheta * o.rho_max;
     }
 
     // Clean up
@@ -147,18 +240,19 @@ void usage()
     printf( "\nOTHER OPTIONS:\n" );
     printf( "  -f  format   The output format string. For complete list of "
                            "format specifiers, see the man page. (default: "
-                           "\"Px Py Pz Bx By Bz\")\n" );
+                           "\"Xx Xy Xz Bx By Bz\")\n" );
     printf( "  -g  gt       The grid type; \"gt\" can be \"cart\", \"cyl2\", "
                            "or \"cyl3\" (default: \"cart\")\n" );
     printf( "  -h           Display this help and exit\n" );
+    printf( "  -l           Limit to points within light cylinder\n" );
     printf( "  -L           Normalise distances to light cylinder radius\n" );
     printf( "  -N  npoints  The number of points along a side of the grid.\n"
-            "               With grid type \"cart\" or \"cyl3\", this "
-                           "effectively sets the grid spacing size to be "
-                           "2*rho*rL/(npoints - 1).\n"
-            "               With grid type \"cyl2\", the grid spacing is set "
-                           "to be 2*pi*rho/npoints.\n"
-            "               (default: 11 for cart/cyl3, 24 for cyl2)\n");
+            "               With grid type \"cart\", this effectively sets "
+                           "the grid spacing size to be 2*rho*rL/(npoints - "
+                           "1).\n"
+            "               With grid type \"cyl2\" or \"cyl3\", the grid "
+                           "spacing is set to be 2*pi*rho/npoints.\n"
+            "               (default: 11 for cart, 24 for cyl2/cyl3)\n");
     printf( "  -o  outfile  The name of the output file to write to. If not "
                            "set, output will be written to stdout.\n" );
     printf( "  -r  rho      The largest rho to consider, as a fraction of "
@@ -174,7 +268,7 @@ void parse_cmd_line( int argc, char *argv[], struct opts *o )
 {
     // Collect the command line arguments
     int c;
-    while ((c = getopt( argc, argv, "a:f:g:hLN:o:P:r:v:z:")) != -1)
+    while ((c = getopt( argc, argv, "a:f:g:hlLN:o:P:r:v:z:")) != -1)
     {
         switch (c)
         {
@@ -201,6 +295,9 @@ void parse_cmd_line( int argc, char *argv[], struct opts *o )
             case 'h':
                 usage();
                 exit(EXIT_SUCCESS);
+                break;
+            case 'l':
+                o->rL_lim = 1;
                 break;
             case 'L':
                 o->rL_norm = 1;
@@ -243,13 +340,119 @@ void parse_cmd_line( int argc, char *argv[], struct opts *o )
     }
 
     if (o->format == NULL)
-        o->format = strdup("Px Py Pz Bx By Bz");
+        o->format = strdup("Xx Xy Xz Bx By Bz");
 
     if (o->npoints == 0)
     {
-        if (o->grid_type == GT_CART || o->grid_type == GT_CYL3)
+        if (o->grid_type == GT_CART)
             o->npoints = 11;
         else
             o->npoints = 24;
     }
+}
+
+
+void parse_format( char *format, struct tokens *tok )
+{
+    char c;
+    int char_num = 0;
+    tok->n = 0;
+    do
+    {
+        c = *format;
+        if (char_num == 0) // Parse the first character
+        {
+            switch (c)
+            {
+                case 'A':
+                    tok->calcA = 1;
+                    __attribute__ ((fallthrough));
+                case 'V':
+                    tok->calcV = 1;
+                    __attribute__ ((fallthrough));
+                case 'B':
+                    tok->calcB = 1;
+                    __attribute__ ((fallthrough));
+                case 'X':
+                    tok->token[tok->n][char_num] = c;
+                    char_num++;
+                    break;
+                default:
+                    // char_num stays at 0
+                    break;
+            }
+        }
+        else if (char_num == 1) // Parse the 2nd character
+        {
+            switch (c)
+            {
+                case 'x':
+                case 'y':
+                case 'z':
+                    tok->token[tok->n][char_num] = c;
+                    if (tok->token[tok->n][0] == 'X' ||
+                        tok->token[tok->n][0] == 'B')
+                    {
+                        char_num = 0;
+                        tok->n++;
+                    }
+                    else
+                        char_num++;
+                    break;
+                case 'd': // only allowed after 'B'
+                    if (tok->token[tok->n][0] == 'B')
+                    {
+                        tok->token[tok->n][char_num] = c;
+                        char_num++;
+                    }
+                    else
+                        char_num = 0;
+                    break;
+                default:
+                    char_num = 0;
+                    break;
+            }
+        }
+        else if (char_num == 2) // Parse the 3rd character
+        {
+            switch (c)
+            {
+                case 'P':
+                case 'N':
+                    if (tok->token[tok->n][0] == 'V')
+                    {
+                        tok->calcV = 1;
+                        tok->calcB = 1;
+                        tok->token[tok->n][char_num] = c;
+                        tok->n++;
+                    }
+                    if (tok->token[tok->n][0] == 'A')
+                    {
+                        tok->calcA = 1;
+                        tok->calcV = 1;
+                        tok->calcB = 1;
+                        tok->token[tok->n][char_num] = c;
+                        tok->n++;
+                    }
+                    break;
+                case 'R':
+                    if (tok->token[tok->n][0] == 'B' &&
+                        tok->token[tok->n][1] == 'd')
+                    {
+                        tok->token[tok->n][char_num] = c;
+                        tok->n++;
+                        tok.calcB = 1;
+                    }
+            }
+            char_num = 0;
+        }
+        format++;
+    } while (c != '\0' && tok->n < MAX_NTOKENS);
+}
+
+
+void print_col_headers( FILE *f, char *format )
+{
+    // Print out a line to file handle f
+    fprintf( f, "# %s\n", format );
 }
